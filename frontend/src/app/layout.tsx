b25591e7b53052eac8ecbcf047bb61b450766f0e
Salaheddine El Mokhtari <salaheddine@elmokhtari.com>
2025-05-23 09:20:47 +0200

    feat(develop): add access denied page to redirect users without permission

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,26 +1,25 @@
-// src/app/layout.tsx (Next.js App Router)
-"use client";
 import React from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import Header from "@/components/Header";
 import theme from "@/theme";
 import { Providers } from "@/store/providers";
-import 'leaflet/dist/leaflet.css';
+import "leaflet/dist/leaflet.css";
 import AuthInitializer from "@/components/AuthInitializer";
 
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body style={{ margin: 0, padding: 0 }}>
         <Providers>
-        <ThemeProvider theme={theme}>
-        <AuthInitializer />
-          <Header />
-          <main style={{ paddingTop: "80px" }}>
-            {children}
-          </main>
-        </ThemeProvider>
+          <ThemeProvider theme={theme}>
+            <AuthInitializer />
+            <Header />
+            <main style={{ paddingTop: "80px" }}>{children}</main>
+          </ThemeProvider>
         </Providers>
       </body>
     </html>
--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,4 +1,4 @@
-
+'use client';
 // src/theme.ts
 import { createTheme } from '@mui/material/styles';
 
